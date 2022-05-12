94f51a2addbfa544b93d85bde5136e2c1e378618
samergain <samersadon@gmail.com>
2022-05-12 13:02:01 -0700

    created a movie card partial and implemented it in home index page

--- Infrastructure/Repositories/MovieRepository.cs
+++ Infrastructure/Repositories/MovieRepository.cs
@@ -19,7 +19,22 @@ namespace Infrastructure.Repositories
             //Dapper 
             // Entity Framework Core => LINQ
             // select top(30) * from movie order by Revenue
-            throw new NotImplementedException();
+            var movies = new List<Movie>()
+            {
+                new Movie { Id = 1, Title = "Inception", PosterUrl = "https://www.themoviedb.org/t/p/original/edv5CZvWj09upOsy2Y6IwDhK8bt.jpg"},
+                new Movie { Id = 2, Title = "Inception", PosterUrl = "https://www.themoviedb.org/t/p/original/edv5CZvWj09upOsy2Y6IwDhK8bt.jpg"},
+                new Movie { Id = 3, Title = "Inception", PosterUrl = "https://www.themoviedb.org/t/p/original/edv5CZvWj09upOsy2Y6IwDhK8bt.jpg"},
+                new Movie { Id = 4, Title = "Inception", PosterUrl = "https://www.themoviedb.org/t/p/original/edv5CZvWj09upOsy2Y6IwDhK8bt.jpg"},
+                new Movie { Id = 5, Title = "Inception", PosterUrl = "https://www.themoviedb.org/t/p/original/edv5CZvWj09upOsy2Y6IwDhK8bt.jpg"},
+                new Movie { Id = 6, Title = "Inception", PosterUrl = "https://www.themoviedb.org/t/p/original/edv5CZvWj09upOsy2Y6IwDhK8bt.jpg"},
+                new Movie { Id = 7, Title = "Inception", PosterUrl = "https://www.themoviedb.org/t/p/original/edv5CZvWj09upOsy2Y6IwDhK8bt.jpg"},
+                new Movie { Id = 8, Title = "Inception", PosterUrl = "https://www.themoviedb.org/t/p/original/edv5CZvWj09upOsy2Y6IwDhK8bt.jpg"},
+                new Movie { Id = 9, Title = "Inception", PosterUrl = "https://www.themoviedb.org/t/p/original/edv5CZvWj09upOsy2Y6IwDhK8bt.jpg"},
+                new Movie { Id = 10, Title = "Inception", PosterUrl = "https://www.themoviedb.org/t/p/original/edv5CZvWj09upOsy2Y6IwDhK8bt.jpg"},
+                new Movie { Id = 11, Title = "Inception", PosterUrl = "https://www.themoviedb.org/t/p/original/edv5CZvWj09upOsy2Y6IwDhK8bt.jpg"},
+                new Movie { Id = 12, Title = "Inception", PosterUrl = "https://www.themoviedb.org/t/p/original/edv5CZvWj09upOsy2Y6IwDhK8bt.jpg"}
+            };
+            return movies;
         }
     }
 }
