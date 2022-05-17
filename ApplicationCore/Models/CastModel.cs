a7607cfad3809d2451166697ac0a306d3f1946cc
samergain <samersadon@gmail.com>
2022-05-16 19:06:28 -0700

    Home page is working and getting data from db

---
+++ ApplicationCore/Models/CastModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Models
+{
+    public class CastModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ProfilePath { get; set; }
+        public string Character { get; set; }
+    }
+}
---
+++ ApplicationCore/Models/MovieDetailsModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Models
+{
+    public class MovieDetailsModel
+    {
+        public MovieDetailsModel()
+        {
+            Genres = new List<GenreModel>();
+            Trailers = new List<TrailerModel>();
+            Casts = new List<CastModel>();
+
+        }
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? PosterUrl { get; set; }
+        public string? BackdropUrl { get; set; }
+        public decimal? Rating { get; set; }
+        public string? Overview { get; set; }
+        public string? Tagline { get; set; }
+        public decimal? Budget { get; set; }
+        public decimal? Revenue { get; set; }
+        public string? ImdbUrl { get; set; }
+        public string? TmdbUrl { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+        public int? RunTime { get; set; }
+        public decimal? Price { get; set; }
+        public List<GenreModel> Genres { get; set; }
+        public List<TrailerModel> Trailers { get; set; }
+        public List<CastModel> Casts { get; set; }
+
+    }
+}
