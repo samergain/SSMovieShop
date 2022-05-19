897c13c0bfc51db9cd548f9c8cd7937d7939791b
samergain <samersadon@gmail.com>
2022-05-19 14:42:11 -0700

    implemented Authorize filter on userController to secure user pages

--- MovieShopMVC/Controllers/UserController.cs
+++ MovieShopMVC/Controllers/UserController.cs
@@ -1,19 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace MovieShopMVC.Controllers
 {
+    [Authorize]
     public class UserController : Controller
     {
         [HttpGet]
+        
         public async Task<IActionResult> Purchases()
         {
             //check if user is logged in (if not logged in -> redirect to login page)
             //get userId -> go to purchase table and get user's purchased movies
             //display as movie cards
+
+            // Purchases are secured resource and to be accessed we should:
+            // 1- var data = this.HttpContext.Request.Cookies["MovieShopAuthCookie"];
+            // 2- decrypt the cookie and get the userid from claims and expiration time from the cookie 
+            // 3- use the userid to go to db and get the movies purchased
+            // the steps above are automatically done in asp.net core using middleware in program.cs
+
+            // we can achieve the isLoggedIn using Filters
+            //var isLoggedIn = this.HttpContext.User.Identity.IsAuthenticated;
+            //if (!isLoggedIn)
+            //{
+            //    //redirect to login page
+            //}
+            //var userId = this.HttpContext.User.Claims.FirstOrDefault(x => x.ValueType == ClaimType.Identifiers)?.Value;
+            var userId = Convert.ToInt32(this.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value);
+            //call userService -> getPurchasedMovies -> list<moviecards>
             return View();
         }
 
+        [HttpGet]
         public async Task<IActionResult> Favorites()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Reviews()
         {
             return View();
         }
--- MovieShopMVC/Program.cs
+++ MovieShopMVC/Program.cs
@@ -56,6 +56,8 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+//
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
